11e5ca0b8f4190c461660993d7b6a35167c97c58
je-recopuerto <jeffreyrecoputerto@gmail.com>
2025-09-04 02:28:30 +0530

    Solved minor issues

--- BitSorter Frontend/src/utils/axiosClient.js
+++ BitSorter Frontend/src/utils/axiosClient.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 const axiosClient = axios.create({
-  baseURL: 'http://localhost:5000',
+  //baseURL: 'http://localhost:5000',
   //baseURL: 'https://bitsorter.onrender.com',
+  baseURL: 'https://bitsorter-production.up.railway.app',
   withCredentials:true,
   headers: {
     'Content-type': 'application/json'

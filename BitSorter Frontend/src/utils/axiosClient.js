3412adfa631ad8c2e95e1b6018baa496e71f32c1
je-recopuerto <jeffreyrecoputerto@gmail.com>
2025-10-05 14:14:47 +0530

    Update axiosClient.js

--- BitSorter Frontend/src/utils/axiosClient.js
+++ BitSorter Frontend/src/utils/axiosClient.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 const axiosClient = axios.create({
   //baseURL: 'http://localhost:5000',
-  //baseURL: 'https://bitsorter.onrender.com',
-  baseURL: 'https://bitsorter-production.up.railway.app',
+  baseURL: 'https://bitsorter.onrender.com',
+  // baseURL: 'https://bitsorter-production.up.railway.app',
   withCredentials:true,
   headers: {
     'Content-type': 'application/json'

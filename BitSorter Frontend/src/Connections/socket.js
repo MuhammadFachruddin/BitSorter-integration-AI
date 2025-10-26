e3184e146e867d6d0c4a0794813590fdc1e8f7c6
je-recopuerto <jeffreyrecoputerto@gmail.com>
2025-10-26 17:54:09 +0530

    Change socket connection URL to production

--- BitSorter Frontend/src/Connections/socket.js
+++ BitSorter Frontend/src/Connections/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:5000", {
+const socket = io("https://bitsorter-2-0-nnjg.onrender.com", {
   withCredentials: true,
   transports: ['websocket']
 });
